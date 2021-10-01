99b0119d5198ba7e49992c5761cbb149f8104b72
noguchitomoya <noguchi.tomoya.infratop@gmail.com>
2021-10-01 16:04:14 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,17 @@
+
+# ↑jupter notebookで使用する場合のマジックコマンド
+import matplotlib.pyplot as plt
+import networkx as nx
+
+# Graphオブジェクトの作成
+G = nx.Graph()
+
+# nodeデータの追加
+G.add_nodes_from(["A", "B", "C", "D", "E", "F"])
+
+# edgeデータの追加
+G.add_edges_from([("A", "B"), ("B", "C"), ("B", "F"), ("C", "D"), ("C", "E"), ("C", "F"), ("B", "F")])
+
+# ネットワークの可視化
+nx.draw(G, with_labels=True)
+plt.show()
